5d70c5e1e5e1f6cd7bdd65069ce081396729e316
Victor Antoine <contact@victor-antoine.fr>
2013-11-09 12:28:24 +0100

    Updating project
    (for backup purpose mainly)

--- bakeModels.php
+++ bakeModels.php
@@ -1,20 +1,32 @@
 #!/usr/bin/php -q
 <?php
-if($argc < 9)
-    exit("Incorrect syntax : bakeModels.php sgbd host database username password schema folder singularise(Y/N)");
-$sgbd = $argv[1];
-$host = $argv[2];
-$database = $argv[3];
-$username = $argv[4];
-$password = $argv[5];
-$schema = $argv[6];
-$folder = $argv[7];
-$singularise = $argv[8];
+include 'Inflector.class.php';
+echo "SGBD : ";
+$sgbd = trim(fgets(STDIN));
+if(!in_array($sgbd, array("pgsql", "mysql")))
+    die("SGBD unsupported");
+echo "Host (localhost): ";
+$host = trim(fgets(STDIN));
+if(empty($host))
+    $host = "localhost";
+echo "Database : ";
+$database = trim(fgets(STDIN));
+echo "Username : ";
+$username = trim(fgets(STDIN));
+echo "Password : ";
+$password = trim(fgets(STDIN));
 
-$connexion = new PDO("$sgbd:host=$host;dbname=$database", "$username", "$password");
+try {
+    $connexion = new PDO("$sgbd:host=$host;dbname=$database", "$username", "$password");
+}
+catch(Exception $e) {
+    die("Erreur : ".$e->getMessage());
+}
 
-function getTables($connexion, $schema) {
-    switch($connexion->getAttribute(constant("PDO::ATTR_DRIVER_NAME"))) {
+function getTables($connexion) {
+    echo "Schema : ";
+    $schema = trim(fgets(STDIN));
+    switch($connexion->getAttribute(PDO::ATTR_DRIVER_NAME)) {
         case "pgsql":
             $q = $connexion->prepare("SELECT tablename FROM pg_tables WHERE schemaname = :schema");
             $q->bindValue(":schema", $schema);
@@ -46,13 +58,23 @@ function getPrimaryKey($connexion, $tableName) {
     return $primaryKey;
 }
 
-function createFiles($connexion, $tableNames, $folder, $singularise) {
+function createFiles($connexion, $tableNames) {
+    echo "Singularise : ";
+    $singularise = trim(fgets(STDIN));
+    echo "Folder : ";
+    $folder = trim(fgets(STDIN));
+    echo "Override existing files ? (N) : ";
+    $override = trim(fgets(STDIN));
+    if(empty($override))
+        $override = 'N';
     foreach ($tableNames as $table) {        
         $primaryKey = getPrimaryKey($connexion, $table[0]);
+        
         if($singularise == 'Y')
-            $modelName = ucfirst(singularize($table[0]));
+            $modelName = ucfirst(Inflector::singularize($table[0]));
         else
-            $modelName = ucfirst($table[0]);        
+            $modelName = ucfirst($table[0]);      
+        
         if (strpos($modelName, "_") >= 0) {
             $parts = explode("_", $modelName);
             foreach ($parts as $key => $value) {
@@ -60,86 +82,21 @@ function createFiles($connexion, $tableNames, $folder, $singularise) {
             }
             $modelName = implode("", $parts);
         }
-        $content = "<?php\r\n";
-        $content .= "class ".$modelName." extends AppModel {\r\n";
-        $content .= '   public $useTable = "'.$table[0].'";'."\r\n";
-        $content .= '   public $primaryKey = "'.$primaryKey.'";'."\r\n";
-        $content .= '   public $hasOne = array();'."\r\n";
-        $content .= '   public $hasMany = array();'."\r\n";
-        $content .= '   public $belongsTo = array();'."\r\n";
-        $content .= '   public $hasAndBelongsToMany = array();'."\r\n";
-        $content .= '}'."\r\n";
-        $content .= '?>';
-        file_put_contents($folder."/" . $modelName . ".php", $content);
+        if($override == 'N' && !file_exists($folder."/" . $modelName . ".php")) {                
+            $content = "<?php\r\n";
+            $content .= "class ".$modelName." extends AppModel {\r\n";
+            $content .= '   public $useTable = "'.$table[0].'";'."\r\n";
+            $content .= '   public $primaryKey = "'.$primaryKey.'";'."\r\n";
+            $content .= '   public $hasOne = array();'."\r\n";
+            $content .= '   public $hasMany = array();'."\r\n";
+            $content .= '   public $belongsTo = array();'."\r\n";
+            $content .= '   public $hasAndBelongsToMany = array();'."\r\n";
+            $content .= '}'."\r\n";
+            $content .= '?>';
+            file_put_contents($folder."/" . $modelName . ".php", $content);
+        }
     }
 }
 
-/**
-* @author Bermi Ferrer Martinez 
-* @copyright Copyright (c) 2002-2006, Akelos Media, S.L. http://www.akelos.org
-* @license GNU Lesser General Public License 
-* @since 0.1
-* @version $Revision 0.1 $
-*/
-function singularize($word)
-{
-    $singular = array (
-    '/(quiz)zes$/i' => '\1',
-    '/(matr)ices$/i' => '\1ix',
-    '/(vert|ind)ices$/i' => '\1ex',
-    '/^(ox)en/i' => '\1',
-    '/(alias|status)es$/i' => '\1',
-    '/([octop|vir])i$/i' => '\1us',
-    '/(cris|ax|test)es$/i' => '\1is',
-    '/(shoe)s$/i' => '\1',
-    '/(o)es$/i' => '\1',
-    '/(bus)es$/i' => '\1',
-    '/([m|l])ice$/i' => '\1ouse',
-    '/(x|ch|ss|sh)es$/i' => '\1',
-    '/(m)ovies$/i' => '\1ovie',
-    '/(s)eries$/i' => '\1eries',
-    '/([^aeiouy]|qu)ies$/i' => '\1y',
-    '/([lr])ves$/i' => '\1f',
-    '/(tive)s$/i' => '\1',
-    '/(hive)s$/i' => '\1',
-    '/([^f])ves$/i' => '\1fe',
-    '/(^analy)ses$/i' => '\1sis',
-    '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\1\2sis',
-    '/([ti])a$/i' => '\1um',
-    '/(n)ews$/i' => '\1ews',
-    '/s$/i' => '',
-    );
-
-    $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');
-
-    $irregular = array(
-    'person' => 'people',
-    'man' => 'men',
-    'child' => 'children',
-    'sex' => 'sexes',
-    'move' => 'moves');
-
-    $lowercased_word = strtolower($word);
-    foreach ($uncountable as $_uncountable){
-        if(substr($lowercased_word,(-1*strlen($_uncountable))) == $_uncountable){
-            return $word;
-        }
-    }
-
-    foreach ($irregular as $_plural=> $_singular){
-        if (preg_match('/('.$_singular.')$/i', $word, $arr)) {
-            return preg_replace('/('.$_singular.')$/i', substr($arr[0],0,1).substr($_plural,1), $word);
-        }
-    }
-
-    foreach ($singular as $rule => $replacement) {
-        if (preg_match($rule, $word)) {
-            return preg_replace($rule, $replacement, $word);
-        }
-    }
-
-    return $word;
-}
-
-createFiles($connexion, getTables($connexion, $schema), $folder, $singularise);
+createFiles($connexion, getTables($connexion));
 ?>
